82e1509d05345862626765fe6ca170c16664d3b1
Lorenz Buehmann <buehmann@informatik.uni-leipzig.de>
2017-11-19 18:03:57 +0100

    Started workbench view plugin

---
+++ src/main/java/org/dllearner/tools/protege/DLLearnerWorkbenchPlugin.java
@@ -0,0 +1,14 @@
+package org.dllearner.tools.protege;
+
+import org.protege.editor.core.ui.view.ViewComponent;
+
+/**
+ * @author Lorenz Buehmann
+ */
+public class DLLearnerWorkbenchPlugin extends ViewComponent {
+
+    @Override
+    public void initialise() throws Exception {
+
+    }
+}
